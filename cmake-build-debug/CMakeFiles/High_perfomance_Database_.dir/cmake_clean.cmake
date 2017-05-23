fc5cc6f9a3d4791ffe9ee4787f8f0e76eb69b8b2
Vladimir <vlbar@inbox.ru>
2017-05-23 13:26:35 +0300

    Initial

---
+++ cmake-build-debug/CMakeFiles/High_perfomance_Database_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/High_perfomance_Database_.dir/main.cpp.o"
+  "High_perfomance_Database_.pdb"
+  "High_perfomance_Database_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/High_perfomance_Database_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
