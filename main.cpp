82fd91c61bb736a13f655419d4a53f0a0be538c8
Vladimir <vlbar@inbox.ru>
2017-05-23 18:21:53 +0300

    Added command line parameter for help message

--- main.cpp
+++ main.cpp
@@ -1,27 +1,35 @@
 #include "gen/milldb.tab.c"
-#include <iostream>
 #include <fstream>
 
+void process_file(char* filename) {
+	FILE* in = fopen(filename, "r");
+	if (!in) {
+		std::cerr << filename << ": can not open file" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	yyin = in;
+	if (!yyparse())
+		std::cout << "OK" << std::endl;
+	fclose(in);
+}
 
-using namespace std;
+int parse_arguments(int argc, char *argv[]) {
+	++argv, --argc;
 
-// TODO Use this library https://github.com/jarro2783/cxxopts to parse command line arguments
+	if ( (argc == 0) || (argc == 1 && (strcmp(argv[0], "-h") == 0 || strcmp(argv[0], "--help") == 0)) ) {
+		// TODO Write here a help message
+		std::cout << "help" << std::endl;
+	} else if (argc == 1) {
+		char* filename = argv[0];
+		process_file(filename);
+	} else {
+		std::cout << "milldb: invalid options\n" << "Try 'milldb --help' for more information." << std::endl;
+	}
+
+	return 0;
+}
 
 int main(int argc, char *argv[]) {
-    FILE *in;
-    ++argv, --argc;
-
-    if (argc == 1) {
-        in = fopen(argv[0], "r");
-        if (!in) {
-            cerr << argv[0] << ": can not open file" << endl;
-        } else {
-            yyin = in;
-            if (!yyparse()) cout << "OK" << endl;
-            fclose(in);
-        }
-    } else {
-        cout << "incorrect number of arguments" << endl;
-    }
+	parse_arguments(argc, argv);
     return 0;
 }
