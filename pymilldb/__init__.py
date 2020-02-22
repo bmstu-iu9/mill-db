6356efbf62ea9a61a3013d6aba20dbd1551008c9
Denis <chugunov-official@mail.ru>
2020-02-22 23:49:07 +0300

    Add __main__.py

--- pymilldb/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from .main import generate
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        raise Exception('Insert only filename')
-    else:
-        generate(sys.argv[1])
