f5feecea6e16be957f034fe401767bdf043f5f8f
Denis <chugunov-official@mail.ru>
2020-02-22 22:40:13 +0300

    Test

--- setup.py
+++ setup.py
@@ -4,17 +4,14 @@ import os
 setup(
     name='pymilldb',
     version='0.1',
-    package_dat={
-        'pymilldb': [
-            'pymilldb/logging.yaml',
-            'pymilldb/template/*.c',
-            'pymilldb/template/*.h'
-        ]
-    },
-    package=[
-        'pymilldb',
-        'pymilldb.context'
-    ],
+    # package_dat={
+    #     'pymilldb': [
+    #         'pymilldb/logging.yaml',
+    #         'pymilldb/template/*.c',
+    #         'pymilldb/template/*.h'
+    #     ]
+    # },
+    package=find_packages(),
     install_requires=[
         'pyyaml>=5.1.2',
         'jinja2>=2.10.3',
